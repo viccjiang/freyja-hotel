dc2c5adc395a8f031cae36484ba5adca73f257ab
jiangvicc <binbinbb520@gmail.com>
2024-09-28 11:48:34 +0800

    feat: swagger https

--- develop/swagger.js
+++ develop/swagger.js
@@ -2,6 +2,8 @@ import swaggerAutogenous from 'swagger-autogen';
 import path from 'path';
 
 const doc = {
+    host: 'freyja-hotel.onrender.com',
+    schemes: ['https'],
     info: {
         title: 'Freyja 旅館 API 系統 | 六角學院',
         description: `打破影音課程售後不理，我們在線上等著你。\n注意事項：登入成功後請點「Authorize」輸入 Token。\n\n範例程式碼 :
